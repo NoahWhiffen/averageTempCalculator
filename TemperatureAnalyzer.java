b8506c8457d542f1d930d4a1d6a7b4c68e96208f
Noah Whiffen <Noah.Whiffen@keyin.com>
2025-05-07 20:00:13 -0230

    Variables and loop created, along with formula for average

--- TemperatureAnalyzer.java
+++ TemperatureAnalyzer.java
@@ -2,21 +2,34 @@
 // Average Temp Calculator
 // May 7th, 2025
 
-import java.Util.Scanner;
+import java.util.Scanner;
 
 public class TemperatureAnalyzer {
     public static void main(String[] args) {
         
         // Take an input from the user (number of days)
         Scanner scanner = new Scanner(System.in);
-    
+        
         System.out.print("Enter number of days: ");
+        
+        // Initialize necessary variables
+        int days = scanner.nextInt();
+        int[] temperatures = new int[days];
+        int sum = 0;
 
-    // Prompt the user to enter all temperature values
+        // Prompt the user to enter all temperature values
+        for (int i = 0; i < days; i++) {
+            System.out.print("Enter temperature for day " + (i + 1) + ": ");
+            temperatures[i] = scanner.nextInt();
+            sum += temperatures[i];
+        }
 
-    // Calculate the average temperature
+        // Calculate the average temperature
+        double average = (double) sum / days;
+        System.out.println("Average temperature: " + average);
 
     // Using the average, Find how many temps are higher than average
     
+    scanner.close();
     }
 }

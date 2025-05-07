52179e696aa98baac4b99a65086c27665eaac1a8
Noah Whiffen <Noah.Whiffen@keyin.com>
2025-05-07 10:04:49 -0230

    Add files via upload

---
+++ TemperatureAnalyzer.java
@@ -0,0 +1,22 @@
+// Noah Whiffen - SD 12
+// Average Temp Calculator
+// May 7th, 2025
+
+import java.Util.Scanner;
+
+public class TemperatureAnalyzer {
+    public static void main(String[] args) {
+        
+        // Take an input from the user (number of days)
+        Scanner scanner = new Scanner(System.in);
+    
+        System.out.print("Enter number of days: ");
+
+    // Prompt the user to enter all temperature values
+
+    // Calculate the average temperature
+
+    // Using the average, Find how many temps are higher than average
+    
+    }
+}
